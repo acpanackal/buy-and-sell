a12e144b5c3cf75c68bbc43df698d01be69ccfa7
Akhila Panackal <akhilacyriacpanackal@gmail.com>
2021-01-30 22:15:20 +0100

    Adding Pages and dummy data and some styles

---
+++ src/app/listings-detail-page/listings-detail-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listings-detail-page',
+  templateUrl: './listings-detail-page.component.html',
+  styleUrls: ['./listings-detail-page.component.scss']
+})
+export class ListingsDetailPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/types.ts
@@ -0,0 +1,8 @@
+import { Interface } from 'readline';
+
+export interface Listing {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+}
